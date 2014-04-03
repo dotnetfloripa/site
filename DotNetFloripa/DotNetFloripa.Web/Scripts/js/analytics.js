7f0a8f8975832c59e4452f8cb92a85adeb30560a
Luís Rudge <luis@luisrudge.net>
2014-04-03 16:05:53 -0300

    Update analytics.js

--- DotNetFloripa/DotNetFloripa.Web/Scripts/js/analytics.js
+++ DotNetFloripa/DotNetFloripa.Web/Scripts/js/analytics.js
@@ -7,3 +7,9 @@
 
 ga('create', 'UA-47847572-1', 'dotnetfloripa.com.br');
 ga('send', 'pageview');
+
+
+window.appInsights={queue:[],applicationInsightsId:null,accountId:null,appUserId:null,configUrl:null,start:function(n){function u(n,t){n[t]=function(){var i=arguments;n.queue.push(function(){n[t].apply(n,i)})}}function f(n){var t=document.createElement("script");return t.type="text/javascript",t.src=n,t.async=!0,t}function r(){i.appendChild(f("//az416426.vo.msecnd.net/scripts/ai.0.js"))}var i,t;this.applicationInsightsId=n;u(this,"logEvent");u(this,"logPageView");i=document.getElementsByTagName("script")[0].parentNode;this.configUrl===null?r():(t=f(this.configUrl),t.onload=r,t.onerror=r,i.appendChild(t));this.start=function(){}}};
+
+appInsights.start("e25457b3-55b2-4b61-b930-f5e5dc45545e");
+appInsights.logPageView();
